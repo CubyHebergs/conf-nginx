fa288dafeae03ede3893c74b4b5b095b36b0239d
vBlackOut <vladimir.souchet@cuby-hebergs.com>
2017-07-10 14:47:00 +0200

    Update script_install.sh

--- script_install.sh
+++ script_install.sh
@@ -2,6 +2,6 @@ wget 'http://nginx.org/download/nginx-1.11.2.tar.gz'
 wget 'https://www.openssl.org/source/openssl-1.0.2h.tar.gz'
 
 cd nginx-1.11.2
-./configure --prefix=/opt/nginx --add-module=/home/source/headers-more-nginx-module --add-module=/home/source/ngx_brotli --with-http_ssl_module --with-http_v2_module --with-http_realip_module --with-http_stub_status_module --with-ipv6 --with-openssl=/home/source/openssl-1.0.2h
+./configure --prefix=/opt/nginx --add-module=/home/source/headers-more-nginx-module --add-module=/home/source/ngx_brotli --with-http_ssl_module --with-http_v2_module --with-http_realip_module --with-http_stub_status_module --with-ipv6 --with-openssl=/home/source/openssl-1.0.2h --without-http_referer_module
 make
 make install
